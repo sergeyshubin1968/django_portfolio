4a248e5976cda3f02802cb112f30f382d164126c
Сергей Шубин <shubin21@yandex.ru>
2023-11-04 19:36:18 +0300

    04 nov for PythonAnywhere

---
+++ portfolio/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Project
+
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects})
+
+
+def blog(request):
+    return render(request, 'blog/all_blogs.html', {'blog':blog})
